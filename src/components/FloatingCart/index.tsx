d9bb7969b2b0bbe31b6309126d2a254da23ce23d
Fellipe Cechinel <fellipecechinel@gmail.com>
2020-08-08 20:00:16 -0300

    Added codes to FloatingCart to sum cart total price and total itens

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useMemo } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 
@@ -24,15 +24,20 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const totalPrice = products.reduce((previousValue, currentProduct) => {
+      const currentPrice = currentProduct.price * currentProduct.quantity;
+      return previousValue + currentPrice;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(totalPrice);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalItens = products.reduce((previousValue, currentProduct) => {
+      return previousValue + currentProduct.quantity;
+    }, 0);
 
-    return 0;
+    return totalItens;
   }, [products]);
 
   return (
