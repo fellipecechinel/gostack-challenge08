2992bc90f1cd5e17247298224a5cb42c6997bf2b
Fellipe Cechinel <fellipecechinel@gmail.com>
2020-08-08 20:00:54 -0300

    Added codes to Cart Page to call cart increment, decrement, and sum cart total price and itens

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,28 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalPrice = products.reduce((previousValue, currentProduct) => {
+      const currentPrice = currentProduct.price * currentProduct.quantity;
+      return previousValue + currentPrice;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(totalPrice);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalItens = products.reduce((previousValue, currentProduct) => {
+      return previousValue + currentProduct.quantity;
+    }, 0);
 
-    return 0;
+    return totalItens;
   }, [products]);
 
   return (
